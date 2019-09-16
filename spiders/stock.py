37d304cf0080a9c37666a893705842ce121f16a1
Poonk <panzhenwei148@vip.qq.com>
2019-09-16 21:33:11 +0800

    update

--- spiders/stock.py
+++ spiders/stock.py
@@ -29,94 +29,158 @@ class StockSpider(scrapy.Spider):
         #     # f.write(codecs.BOM_UTF8)
         #     csv.writer(f).writerow(['日期','收盘价','涨跌幅','资金净流入','5日主力净额','净额','净占比','净额','净占比','净额','净占比'])
 
-        # t = response.xpath('/html/body/div[10]/div[6]/table/tr[last()]/td[1]/text()').extract()[0]
-        # print(t)
-        # t = tr[last()]
-        # (.m_table_3 tr).length
-        # for j in range(3,33) :
-            # content = response.xpath('/html/body/div[10]/div[6]/table/tr' + '[' + str(j) + ']')
-        trs = response.xpath("//table[@class='m_table_3']//tr[position()>2]")
+        trs = response.xpath("//*[@id='history_table_free']/table[@class='m_table_3']//tr[position()>2]")
+        data = []
         for tr in trs:
             date = tr.xpath(".//td[1]/text()").extract()[0]
-            closePrice = tr.xpath('td[2]/text()').extract()[0]
-            quoteChange = tr.xpath('td[3]/text()').extract()[0]
-            inflow = tr.xpath('td[4]/text()').extract()[0]
-            mainForce = tr.xpath('td[5]/text()').extract()[0]
-            bigSingle = tr.xpath('td[6]/text()').extract()[0]
-            bigSingle1 = tr.xpath('td[7]/text()').extract()[0]
-            mediumSingle = tr.xpath('td[8]/text()').extract()[0]
-            mediumSingle1 = tr.xpath('td[9]/text()').extract()[0]
-            smallSingle = tr.xpath('td[10]/text()').extract()[0]
-            smallSingle1 = tr.xpath('td[11]/text()').extract()[0]
+            closePrice = tr.xpath('.//td[2]/text()').extract()[0]
+            quoteChange = tr.xpath('.//td[3]/text()').extract()[0]
+            inflow = tr.xpath('.//td[4]/text()').extract()[0]
+            mainForce = tr.xpath('.//td[5]/text()').extract()[0]
+            bigSingle = tr.xpath('.//td[6]/text()').extract()[0]
+            bigSingle1 = tr.xpath('.//td[7]/text()').extract()[0]
+            mediumSingle = tr.xpath('.//td[8]/text()').extract()[0]
+            mediumSingle1 = tr.xpath('.//td[9]/text()').extract()[0]
+            smallSingle = tr.xpath('.//td[10]/text()').extract()[0]
+            smallSingle1 = tr.xpath('.//td[11]/text()').extract()[0]
 
             crawl_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
             print(date,closePrice,quoteChange,inflow,mainForce,bigSingle,bigSingle1,mediumSingle,mediumSingle1,smallSingle,smallSingle1)
 
-            item = Funds(stock_code=stock_code,
-                stock_market=stock_market,
-                stock_name=stock_name,
-                date = date,
-                closePrice = closePrice,
-                quoteChange = quoteChange,
-                inflow = inflow,
-                mainForce = mainForce,
-                bigSingle = bigSingle,
-                bigSingle1 = bigSingle1,
-                mediumSingle = mediumSingle,
-                mediumSingle1 = mediumSingle1,
-                smallSingle = smallSingle,
-                smallSingle1 = smallSingle1,
-                crawl_time = crawl_time)
-            yield item
+            dataMap = {
+                'date':date,
+                'closePrice' : closePrice,
+                'quoteChange' : quoteChange,
+                'inflow' : inflow,
+                'mainForce' : mainForce,
+                'bigmap':{
+                    'bigSingle':bigSingle,
+                    'bigSingle1':bigSingle1,
+                },
+                'mediumMap':{
+                    'mediumSingle':mediumSingle,
+                    'mediumSingle1':mediumSingle1,
+                },
+                'smallMap':{
+                    'smallSingle':smallSingle,
+                    'smallSingle1':smallSingle1,
+                }
+            }
+            data.append(dataMap)
 
-            for i in self.A_stocks.index:
-                stock_code = self.A_stocks['code'][i]
-                stock_market = self.A_stocks['market'][i]
-                stock_name = self.A_stocks['name'][i]
-                url = 'http://stockpage.10jqka.com.cn/%s/funds/' % stock_code
-                yield scrapy.Request(url=url, callback=self.parse_next, dont_filter=True,
-                                    meta={"info": (stock_code, stock_market, stock_name)})
+        
+            # item = Funds(stock_code=stock_code,
+            #     stock_market=stock_market,
+            #     stock_name=stock_name,
+            #     data = data
+            #     )
+            # yield item
+
+            
+
+            # for i in self.A_stocks.index:
+            #     stock_code = self.A_stocks['code'][i]
+            #     stock_market = self.A_stocks['market'][i]
+            #     stock_name = self.A_stocks['name'][i]
+            #     url = 'http://stockpage.10jqka.com.cn/%s/funds/' % stock_code
+            #     yield scrapy.Request(url=url, callback=self.parse_next, dont_filter=True,
+            #                         meta={"info": (stock_code, stock_market, stock_name)})
 
                 # with open('/mnt/d/Crawler/'+ title[2:len(title)-2] + ".csv",'a',encoding='utf-8') as f:
                 #     csv.writer(f).writerow([date,closePrice,quoteChange,inflow,mainForce,bigSingle,bigSingle1,mediumSingle,mediumSingle1,smallSingle,smallSingle1])
+        for i in range(len(data)):
+            print(data[i])
 
-    def parse_next(self,response):    
-        stock_code, stock_market, stock_name = response.meta.get('info')
+    # def parse_next(self,response):    
+    #     stock_code, stock_market, stock_name = response.meta.get('info')
 
-        trs = response.xpath("//table[@class='m_table_3']//tr[position()>2]")
-        for tr in trs:
-            date = tr.xpath(".//td[1]/text()").extract()[0]
-            closePrice = tr.xpath('td[2]/text()').extract()[0]
-            quoteChange = tr.xpath('td[3]/text()').extract()[0]
-            inflow = tr.xpath('td[4]/text()').extract()[0]
-            mainForce = tr.xpath('td[5]/text()').extract()[0]
-            bigSingle = tr.xpath('td[6]/text()').extract()[0]
-            bigSingle1 = tr.xpath('td[7]/text()').extract()[0]
-            mediumSingle = tr.xpath('td[8]/text()').extract()[0]
-            mediumSingle1 = tr.xpath('td[9]/text()').extract()[0]
-            smallSingle = tr.xpath('td[10]/text()').extract()[0]
-            smallSingle1 = tr.xpath('td[11]/text()').extract()[0]
+    #     trs = response.xpath("//table[@class='m_table_3']//tr[position()>2]")
+    #     data = []
 
-            crawl_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    #     for tr in trs:
+    #         date = tr.xpath(".//td[1]/text()").extract()[0]
+    #         closePrice = tr.xpath('td[2]/text()').extract()[0]
+    #         quoteChange = tr.xpath('td[3]/text()').extract()[0]
+    #         inflow = tr.xpath('td[4]/text()').extract()[0]
+    #         mainForce = tr.xpath('td[5]/text()').extract()[0]
+    #         bigSingle = tr.xpath('td[6]/text()').extract()[0]
+    #         bigSingle1 = tr.xpath('td[7]/text()').extract()[0]
+    #         mediumSingle = tr.xpath('td[8]/text()').extract()[0]
+    #         mediumSingle1 = tr.xpath('td[9]/text()').extract()[0]
+    #         smallSingle = tr.xpath('td[10]/text()').extract()[0]
+    #         smallSingle1 = tr.xpath('td[11]/text()').extract()[0]
 
-            item = Funds(stock_code=stock_code,
-                    stock_market=stock_market,
-                    stock_name=stock_name,
-                    date = date,
-                    closePrice = closePrice,
-                    quoteChange = quoteChange,
-                    inflow = inflow,
-                    mainForce = mainForce,
-                    bigSingle = bigSingle,
-                    bigSingle1 = bigSingle1,
-                    mediumSingle = mediumSingle,
-                    mediumSingle1 = mediumSingle1,
-                    smallSingle = smallSingle,
-                    smallSingle1 = smallSingle1,
-                    crawl_time = crawl_time)
-            yield item
+    #         crawl_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    #         dataMap = {
+    #             'date':date,
+    #             'closePrice' : closePrice,
+    #             'quoteChange' : quoteChange,
+    #             'inflow' : inflow,
+    #             'mainForce' : mainForce,
+    #             'bigmap':{
+    #                 'bigSingle':bigSingle,
+    #                 'bigSingle1':bigSingle1,
+    #             },
+    #             'mediumMap':{
+    #                 'mediumSingle':mediumSingle,
+    #                 'mediumSingle1':mediumSingle1,
+    #             },
+    #             'smallMap':{
+    #                 'smallSingle':smallSingle,
+    #                 'smallSingle1':smallSingle1,
+    #             }
+    #         }
+    #         data.append(dataMap)
+
+    #         item = Funds(stock_code=stock_code,
+    #                 stock_market=stock_market,
+    #                 stock_name=stock_name,
+    #                 data = data)
+    #         yield item
 
         pass
-    # /html/body/div[10]/div[6]/table/tbody/tr[3]/td[2]
-    # /html/body/div[10]/div[6]/table/tbody/tr[3]/td[1]
+
+
+-25144.46
+39385.23
+-193.02
+-9664.10
+-8774.44
+26807.72
+46804.81
+32814.00
+-20948.81
+38542.70
+-2988.60
+-12828.07
+-8635.98
+7640.85
+-30520.41
+51356.16
+-9810.46
+-68143.78
+40903.43
+-2993.77
+10243.47
+19490.68
+10614.70
+-52185.07
+60436.45
+18344.51
+69984.94
+18807.50
+11599.34
+-36053.88
+-25806.86
+38654.88
+823.01
+-9664.10
+-18629.74
+24531.85
+50630.97
+31705.35
+-21248.56
+38149.33
+-2988.60
