8ade5dcd8939b8a6b87ea38193bbe9e37c99cd8e
Poonk <panzhenwei148@vip.qq.com>
2019-09-19 21:35:07 +0800

    use selenium in scrapy

--- spiders/index.py
+++ spiders/index.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from tutorial.items import Index
+from selenium import webdriver
 
 class IndexSpider(scrapy.Spider):
     name = 'index'
     allowed_domains = ['http://stockpage.10jqka.com.cn/HK8238/']
     start_urls = ['http://stockpage.10jqka.com.cn/HK8238//']
 
+     
     def parse(self, response):
-        industry = response.xpath("/html/body/div[7]/div/ul[2]/li[1]/a/text()").extract()[0]
-        marketValue = response.xpath("/html/body/div[7]/div/ul[2]/li[2]/text()")
-        circulationValue = response.xpath("/html/body/div[7]/div/ul[2]/li[3]/strong/text()").extract()[0]
-        PER = response.xpath("/html/body/div[7]/div/ul[2]/li[4]/strong/text()").extract()[0]
-        PBR = response.xpath("/html/body/div[7]/div/ul[2]/li[5]/strong").extract()[0]
-        capitalAmount = response.xpath("/html/body/div[9]/div[6]/div[3]/dl/dd[1]/text()").extract()[0]
-        gain = response.xpath("/html/body/div[9]/div[6]/div[3]/dl/dd[3]/text()").extract()[0]
+        industry = response.xpath("//ul[@class='detail_list cb fl']/li/a/text()").extract()[0]
+        marketValue = response.xpath("//ul[@class='detail_list cb fl']/li[2]/strong/text()").extract()[0]
+        circulationValue = response.xpath("//ul[@class='detail_list cb fl']/li[3]/strong/text()").extract()[0]
+        PER = response.xpath("//ul[@class='detail_list cb fl']/li[4]/strong/text()").extract()[0]
+        PBR = response.xpath("//ul[@class='detail_list cb fl']/li[5]/strong/text()").extract()[0]
+        capitalAmount = response.xpath("//div[@class='sub_cont_3']/dl/dd[1]/text()").extract()[0]
+        gain = response.xpath("//div[@class='sub_cont_3']/dl/dd[3]/text()").extract()[0]
 
         print(industry,marketValue,circulationValue,PER,PBR,capitalAmount,gain)
+        # print(industry,marketValue)
 
         pass
